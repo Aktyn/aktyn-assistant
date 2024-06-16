381e57b0c62639b043ab976d9acabb1a32f6203b
Aktyn <aktyn3@gmail.com>
2024-06-16 16:36:50 +0200

    Fix for tests setup in desktop app

--- apps/desktop/jest.config.js
+++ apps/desktop/jest.config.js
@@ -1,12 +1,12 @@
 const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig.json')
+const { compilerOptions } = require('./tsconfig-app.json')
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['./src'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  roots: ['./src/app'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist-app/', '/dist-ui/'],
   restoreMocks: true,
   clearMocks: true,
   modulePaths: [compilerOptions.baseUrl],
